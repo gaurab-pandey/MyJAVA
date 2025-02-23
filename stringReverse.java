e8507282d07567058646d1ccc6f77e67baa60754
gaurab-pandey <gaurabpandey754@gmail.com>
2025-02-23 23:00:00 +0545

    string reverse

---
+++ stringReverse.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class stringReverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = scanner.nextLine();
+        scanner.close();
+
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        System.out.println("Reversed String: " + reversed);
+    }
+}
