8141c53a945b8540f2c3eced0e5ebbe8ef3c7d68
gaurab-pandey <gaurabpandey754@gmail.com>
2025-03-02 22:22:27 +0545

    PyramidPatter

---
+++ pyramid.java
@@ -0,0 +1,16 @@
+public class pyramid {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 2 * n - 1; j++) {
+                if (j >= n - (i - 1) && j <= n + (i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
