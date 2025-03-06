6c8198f3afd23f5a809a0a97efb03f270a3a0357
gaurab-pandey <gaurabpandey754@gmail.com>
2025-03-06 23:26:04 +0545

    LinearExample

---
+++ LinearExample.java
@@ -0,0 +1,11 @@
+// Example of Linear Time Complexity - O(n)
+
+
+public class LinearExample {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};/*"Iterating through an array	Time increases linearly with input size.." */
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);  // Printing each element (linear time)
+        }
+    }
+}
